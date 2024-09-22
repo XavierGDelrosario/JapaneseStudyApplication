1274035bd574b3b01becc53a7a8e395689ff89fe
Xavier Del Rosario <114903456+XavierGDelrosario@users.noreply.github.com>
2024-09-22 04:00:44 -0700

    Added Discord Bot

---
+++ JapaneseStudyBot/src/Discordbot.py
@@ -0,0 +1,95 @@
+import discord
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import time
+from selenium.common.exceptions import NoSuchElementException
+from KanjiFinder import scrape_kanji_images
+from KanjiFinder import search_images
+from KanjiFinder import NoExamplesException
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+intents = discord.Intents.default()
+intents.messages = True  # Enable message intents
+
+class MyClient(discord.Client):
+    n1_id = 1287322325242482719
+    n2_id = 1287322342195859467
+    n3_id = 1287322367068209195
+    n4_id = 1287322385422352489
+    n5_id = 1287322399485857814
+    def __init__(self, **options):
+        super().__init__(intents=intents, **options)
+        self.scheduler = AsyncIOScheduler()
+        self.scheduler.start()
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+        self.scheduler.add_job(self.send_daily_message, 'cron', hour=12, minute=0)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        time.sleep(1)
+        await message.channel.send("お探しします。少々お待ちください。")
+        if message.content.startswith('!random'):
+            await self.handle_random(message)
+        elif message.content.startswith('!search'):
+            await self.handle_search(message)
+        elif message.content.startswith('!daily'):
+            await self.send_daily_message()
+        else:
+            time.sleep(5)
+            await message.channel.send("その命令がわかりません。申し訳ございません。")
+
+    async def handle_random(self, message):
+        try:
+            message_parts = message.content.split()
+            if len(message_parts) < 2:
+                scrape_kanji_images("All")
+            else:
+                scrape_kanji_images(message_parts[1])
+            await message.channel.send(file=discord.File('./img/kanji_character.png'))
+            await message.channel.send(file=discord.File('./img/kanji_examples.png'))
+        except NoExamplesException as e:
+            await message.channel.send(file=discord.File('./img/kanji_character.png'))
+        except NoSuchElementException as e:
+            await message.channel.send("Invalid command")
+        except Exception as e:
+            await message.channel.send(f"Error occurred: {e}")
+
+    async def handle_search(self, message):
+        try:
+            message_parts = message.content.split()
+            if len(message_parts) < 2:
+                raise ValueError()
+            search_images(message_parts[1])
+            await message.channel.send(file=discord.File('./img/kanji_character.png'))
+            await message.channel.send(file=discord.File('./img/kanji_examples.png'))
+        except ValueError as e:
+            raise
+        except NoExamplesException as e:
+            await message.channel.send(file=discord.File('./img/kanji_character.png'))
+        except NoSuchElementException as e:
+            await message.channel.send("Invalid command")
+        except Exception as e:
+            await message.channel.send(f"Error occurred: {e}")
+
+    async def send_daily_message(self):
+        for i in range(1, 6):  # Loop from 1 to 5
+            channel_id = getattr(self, f'n{i}_id') 
+            channel = self.get_channel(channel_id)
+            if channel:
+                try: 
+                    scrape_kanji_images(f'N{i}')
+                    await channel.send(file=discord.File('./img/kanji_character.png'))
+                    await channel.send(file=discord.File('./img/kanji_examples.png'))
+                except NoExamplesException as e:
+                    await channel.send(file=discord.File('./img/kanji_character.png'))
+                except Exception as e:
+                    print(f'Error occurred: {e}')
+   
+client = MyClient()
+client.run(TOKEN)
---
+++ JapaneseStudyBot/src/KanjiFinder.py
@@ -0,0 +1,197 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+class NoExamplesException(Exception):
+    def __init__(self, message="No examples found."):
+        self.message = message
+        super().__init__(self.message)
+
+import time
+from PIL import Image
+import io
+
+###Variables
+#Drivers
+driver = None 
+default_wait= None
+
+#Info Xpaths
+kanji_png_xpath = '/html/body/div[2]/div[2]/div/div[1]/div/div/div[4]/div/div[1]'
+kanji_character_xpath = '/html/body/div[2]/div[2]/div/div[1]/div/div/div[4]/div/div[position()=last()-1]/span'
+meaning_xpath = '/html/body/div[2]/div[2]/div/div[1]/div/div/div[4]/div/div[1]/table/tbody/tr[2]/td[1]'
+onyomi_xpath = '/html/body/div[2]/div[2]/div/div[1]/div/div/div[4]/div/div[1]/table/tbody/tr[2]/td[2]'
+kunyomi_xpath = '/html/body/div[2]/div[2]/div/div[1]/div/div/div[4]/div/div[1]/table/tbody/tr[2]/td[3]'
+
+#Image Xpaths
+examples_xpath = '/html/body/div[2]/div[2]/div/div[1]/div/div/div[4]/div/div[position()=last()-2]/ul' 
+xpath_image = "/html/body/div[2]/div[2]/div/div[1]/div/div/div[4]/div/div[1]/table/tbody/tr[1]/td/a/img"
+
+#Search
+input_text_xpath = '/html/body/div[2]/div[2]/div/div[1]/div/div/div[3]/form/p[2]/input[1]'
+submit_xpath = '/html/body/div[2]/div[2]/div/div[1]/div/div/div[3]/form/p[2]/input[2]'
+#Image paths
+kanji_png_path = './img/kanji_character.png'
+examples_png_path = './img/kanji_examples.png'
+
+def initialize_driver():
+    global driver
+    global default_wait
+    firefox_options = Options()
+    firefox_options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+    firefox_options.add_argument('--headless')
+    geckodriver_path = './gecko/geckodriver.exe'
+    firefox_service = FirefoxService(executable_path=geckodriver_path)
+    driver = webdriver.Firefox(service=firefox_service, options=firefox_options)
+    default_wait = WebDriverWait(driver, 6)  # Wait up to 10 seconds
+
+#grade can be "All", "N5", "N4", "N3", "N2", "N1", "1", "2", "3", "4", "5", "6"
+def scrape_kanji_info(grade):
+    initialize_driver()
+    driver.get(f'https://www.yookoso.com/study/kanji-study/?grade={grade}')
+    time.sleep(1)
+    try:
+        return get_kanji_info()
+    except NoSuchElementException as e:
+        print(f"Error: {e}")
+        raise NoSuchElementException
+    except Exception as e:
+        print(f"Error: {e}")
+        raise Exception
+    finally:
+        driver.quit()
+
+def scrape_kanji_images(grade):
+    initialize_driver()
+    driver.get(f'https://www.yookoso.com/study/kanji-study/?grade={grade}')
+    time.sleep(1)
+    try:
+        return get_images()
+    except NoExamplesException as e:
+        raise NoExamplesException
+    except NoSuchElementException as e:
+        print(f"Error: {e}")
+        raise NoSuchElementException
+    except Exception as e:
+        print(f"Error: {e}")
+        raise Exception
+    finally:
+        driver.quit()
+
+def search_images(kanji):
+    initialize_driver()
+    driver.get(f'https://www.yookoso.com/study/kanji-study/')
+
+    input_field = default_wait.until(EC.visibility_of_element_located((By.XPATH, input_text_xpath)))
+    input_field.clear()
+    input_field.send_keys(kanji)  
+    submit_button = default_wait.until(EC.element_to_be_clickable((By.XPATH, submit_xpath)))
+    submit_button.click()
+    try:
+        return get_images()
+    except NoExamplesException as e:
+        raise NoExamplesException
+    except NoSuchElementException as e:
+        print(f"Error: {e}")
+        raise NoSuchElementException
+    except Exception as e:
+        print(f"Error: {e}")
+        raise Exception
+    finally:
+        driver.quit()
+
+def search_info(kanji):
+    initialize_driver()
+    driver.get(f'https://www.yookoso.com/study/kanji-study/')
+    input_field = default_wait.until(EC.visibility_of_element_located((By.XPATH, input_text_xpath)))
+    input_field.clear()
+    input_field.send_keys(kanji)  
+    submit_button = default_wait.until(EC.element_to_be_clickable((By.XPATH, submit_xpath)))
+    submit_button.click()
+    try:
+        return get_kanji_info()
+    except NoSuchElementException as e:
+        print(f"Error: {e}")
+        raise NoSuchElementException
+    except Exception as e:
+        print(f"Error: {e}")
+        raise Exception
+    finally:
+        driver.quit()
+
+#assumes driver is loaded and page is correct
+def get_images():
+    result = {}
+    try:
+        kanji_character_png_element = default_wait.until(EC.visibility_of_element_located(("xpath", kanji_png_xpath)))
+        default_wait.until(EC.visibility_of_element_located((By.XPATH, kanji_png_xpath)))
+        driver.execute_script("arguments[0].scrollIntoView();", kanji_character_png_element)
+        time.sleep(1)
+        kanji_character_png_element.screenshot(kanji_png_path)
+        result['kanji_image_path'] = kanji_png_path
+    except NoSuchElementException as e:
+        print(f"Error: {e}")
+        raise NoSuchElementException
+    except Exception as e:
+        print(f"Error: {e}")
+        raise Exception
+    try:
+        examples = default_wait.until(EC.presence_of_all_elements_located(("xpath", f'{examples_xpath}/*')))  # Get all children of the <ul>
+        images = []
+        for ul_element in examples:
+            img_data = ul_element.screenshot_as_png 
+            img = Image.open(io.BytesIO(img_data)) 
+            images.append(img)
+        if len(images) == 0:
+            raise NoExamplesException
+        if images:
+            total_height = sum(img.height for img in images)
+            combined_img = Image.new('RGB', (images[0].width, total_height))
+            current_height = 0
+            for img in images:
+                combined_img.paste(img, (0, current_height))
+                current_height += img.height
+            combined_img.save(examples_png_path)
+            result['kanji_examples_path'] = examples_png_path
+        return result
+    except TimeoutException as e:
+        raise NoExamplesException
+    except NoExamplesException as e:
+        raise NoExamplesException
+    except NoSuchElementException as e:
+        print(f"Error: {e}")
+        raise NoSuchElementException
+    except Exception as e:
+        print(f"Error: {e}")
+        raise Exception
+
+#assumes driver is loaded and page is
+def get_kanji_info():
+    try:
+        kanji_character_text = default_wait.until(EC.presence_of_element_located((By.XPATH, kanji_character_xpath))).text.split('\n')
+        meaning = default_wait.until(EC.presence_of_element_located((By.XPATH, meaning_xpath))).text.split('\n')[1:]
+        onyomi = default_wait.until(EC.presence_of_element_located((By.XPATH, onyomi_xpath))).text.split('\n')[1:]
+        kunyomi = default_wait.until(EC.presence_of_element_located((By.XPATH, kunyomi_xpath))).text.split('\n')[1:]
+
+        kanji_png_element = driver.find_element(By.XPATH, kanji_png_xpath)
+        kanji_png_element.screenshot('kanji_character.png') 
+        return {
+            "Kanji": kanji_character_text,
+            "Meaning": meaning,
+            "Onyomi": onyomi,
+            "Kunyomi": kunyomi
+        }
+    except NoSuchElementException as e:
+        print(f"Error: {e}")
+        raise NoSuchElementException
+    except Exception as e:
+        print(f"Error: {e}")
+        raise Exception
+
+# Test
+#search_images('乃')
+#search_images('絢')
